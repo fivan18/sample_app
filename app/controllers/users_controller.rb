7727d699f4a8450cf71189c0f44bf8ae385d4103
certilremy <certilremy@gmail.com>
2020-02-25 16:44:29 -0500

    added put , create and deleted method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,22 @@ class UsersController < ApplicationController
   def show
     @user = params[:id]
   end
-  
+
   def edit
     @user = params[:id]
   end
 
   def new
-    @name = "Ivan!"
+    @name = 'Ivan!'
+  end
+
+  def create
+    @user = params[:id]
+    redirect_to user_path
+  end
+
+  def delete
+    @user = params[:id]
+    @user.destroy
   end
 end
--- app/script/rest_requestor.rb
+++ app/script/rest_requestor.rb
@@ -3,6 +3,11 @@ puts RestClient.get('http://localhost:3000/users')
 puts RestClient.get('http://localhost:3000/users/1')
 puts RestClient.get('http://localhost:3000/users/2/edit')
 puts RestClient.get('http://localhost:3000/users/new')
+puts RestClient.post('http://localhost:3000/users/create')
+puts RestClient.post('http://localhost:3000/users/delete')
+puts RestClient.put('http://localhost:3000/users')
+
+
 
 
 
