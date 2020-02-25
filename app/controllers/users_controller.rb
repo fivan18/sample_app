ac4418713219c34db5813c41d74b1e0d8701c03d
Ivan <fivanunam@gmail.com>
2020-02-25 15:25:43 -0600

    create remaining 3 GET methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,15 @@
 class UsersController < ApplicationController
   def index; end
+
+  def show
+    @user = params[:id]
+  end
+  
+  def edit
+    @user = params[:id]
+  end
+
+  def new
+    @name = "Ivan!"
+  end
 end
--- app/script/rest_requestor.rb
+++ app/script/rest_requestor.rb
@@ -1,3 +1,9 @@
 require 'rest-client'
 puts RestClient.get('http://localhost:3000/users')
+puts RestClient.get('http://localhost:3000/users/1')
+puts RestClient.get('http://localhost:3000/users/2/edit')
+puts RestClient.get('http://localhost:3000/users/new')
+
+
+
 
